80102ed107d3ca51f172a7bcc87fa238b04f3ba1
kingston <64131655+kingstondav@users.noreply.github.com>
2020-08-06 13:44:11 +0530

    Add files via upload

---
+++ src/components/ProductList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+
+import { ProductConsumer } from "../context";
+export default class ProductList extends Component {
+
+  render() {
+    
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container"></div>
+          <Title name="our " title="products" />
+
+          <div className="row" style={{ padding: "20px" }}>
+            <ProductConsumer>
+              {(value) => {
+                return value.products.map((product) => {
+                  return <Product key={product.id} product={product} />;
+                });
+              }}
+            </ProductConsumer>
+          </div>
+        </div>
+      </React.Fragment>
+      //<Product />
+    );
+  }
+}
